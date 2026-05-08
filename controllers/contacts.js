ab0ad13494178bb4abb1cdc860d7ae6ef1493269
LeoGC7 <lcorrea@byupathway.edu>
2026-05-08 16:03:28 -0600

    W01 Assignment - Part 1

---
+++ controllers/contacts.js
@@ -0,0 +1,37 @@
+const { ObjectId } = require('mongodb')
+const mongodb = require('../db/connect')
+
+const getContacts = async (req, res) => {
+    try {
+        const result = await mongodb.getDatabase().db('cse341').collection('contacts').find()
+
+        const contacts = await result.toArray()
+        res.status(200).json(contacts)
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error fetching contacts',
+            error: err
+        })
+    }
+}
+
+const getContactById = async (req, res) => {
+    try {
+        const contactId = new ObjectId(req.params.id)
+
+        const result = await mongodb.getDatabase().db('cse341').collection('contacts').find({ _id: contactId })
+
+        const contacts = await result.toArray()
+        res.status(200).json(contacts[0])
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error fetching contact',
+            error: err
+        })
+    }
+}
+
+module.exports = {
+    getContacts,
+    getContactById
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+// Requires
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const cors = require('cors')
+const mongodb = require('./db/connect')
+
+// App instance
+const app = express()
+// Port
+const port = process.env.PORT || 8080
+
+// App use statements
+app.use(cors())
+app.use(express.json())
+
+// Basic Route to the / endpoint
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.use('/', require('./routes'))
+
+// Start the app
+mongodb.initDb((err) => {
+    if(err) {
+        console.log("There as an error: " + err)
+    } else {
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port} and DB is connected`)
+        })
+    }
+})
